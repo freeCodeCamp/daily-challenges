6e9712da97a0a36502d4cc38ff30c0507e177019
moT01 <20648924+moT01@users.noreply.github.com>
2025-03-21 09:10:30 -0500

    fix: do not send challenge if it's in the future

--- database/seed.js
+++ database/seed.js
@@ -1,25 +1,15 @@
 import { readFileSync } from 'fs';
-import path, { join } from 'path';
+import { join, resolve, dirname } from 'path';
 import { fileURLToPath } from 'url';
 import dotenv from 'dotenv';
-import { MongoClient, ObjectId } from 'mongodb';
+import { MongoClient } from 'mongodb';
+import { formatDateUsCentral } from '../src/helpers.js';
 
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const __dirname = dirname(__filename);
 
-dotenv.config({ path: path.resolve(__dirname, '../.env') });
+dotenv.config({ path: resolve(__dirname, '../.env') });
 const { MONGO_URI } = process.env;
-
-function formatDateUsCentral(dateObj) {
-  return dateObj
-    .toLocaleString('en-US', {
-      timeZone: 'America/Chicago',
-      year: 'numeric',
-      month: '2-digit',
-      day: '2-digit',
-    })
-    .replace(/\//g, '-');
-}
 
 const seed = async () => {
   const daysToAdd = 7;
@@ -30,26 +20,24 @@ const seed = async () => {
     await client.connect();
     const db = client.db('curriculum');
     const collection = db.collection('dailyChallenges');
-
     await collection.drop();
 
-    // Read the JSON file
-    const baseData = JSON.parse(readFileSync(path.join(__dirname, 'challenges', 'challenge.json'), 'utf-8'));
+    const baseData = JSON.parse(readFileSync(join(__dirname, 'challenges', 'challenge.json'), 'utf-8'));
+    const challenges = [];
 
-    // Insert the object multiple times with different dates
-    const duplicatedData = [];
-    for (let i = 0; i < daysToAdd; i++) {
+    // -1 means it will seed one day into the future
+    for (let i = -1; i < daysToAdd; i++) {
       const date = new Date();
       date.setDate(date.getDate() - i);
 
-      duplicatedData.push({
+      challenges.push({
         ...baseData,
         date: formatDateUsCentral(date),
       });
     }
 
     // Insert data into MongoDB
-    const result = await collection.insertMany(duplicatedData);
+    const result = await collection.insertMany(challenges);
     console.log(`Inserted ${result.insertedCount} challenges`);
   } catch (error) {
     console.error('Error seeding data:', error);
--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import 'dotenv/config';
 import { MongoClient } from 'mongodb';
+import { formatDateUsCentral } from './helpers.js';
 
 const app = express();
 const portNum = process.env.PORT || 3000;
@@ -23,9 +24,13 @@ const dailyChallenges = db.collection('dailyChallenges');
 // Fetch daily challenge by date
 app.get('/api/daily-challenge/date/:date', async (req, res) => {
   const { date } = req.params;
+
   try {
     const challenge = await dailyChallenges.findOne({ date });
-    if (challenge) {
+    const todayUsCentral = formatDateUsCentral(new Date());
+
+    // do not send challenge back if it's in the future (relative to today US Central)
+    if (challenge && challenge.date <= todayUsCentral) {
       res.status(200).json(challenge);
     } else {
       res.status(404).json({ error: 'Challenge not found for this date' });
