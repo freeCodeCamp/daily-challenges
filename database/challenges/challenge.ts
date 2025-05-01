01097927d4bec1a76af8584938636e32d3e74836
Tom <20648924+moT01@users.noreply.github.com>
2025-05-01 16:35:36 -0500

    feat: add first real challenge (#7)

--- database/challenges/challenge.ts
+++ database/challenges/challenge.ts
@@ -1,33 +1,80 @@
 export const baseChallenge = {
-  "challengeId": "5900f36e1000cf542c50fe80",
-  "title": "V  A  P  O  R  W  A  V  E",
-  "description": "<section id=\"description\">\n<p>Given a string, remove all spaces from the string, then insert two spaces between every character and uppercase all letters.</p>\n</section>",
+  "challengeId": "6813c678516e86b171929de3",
+  "title": "Vowel Count Balance",
+  "description": "<p>Given a string, determine whether the number of vowels in the first half of the string is equal to the number of vowels in the second half.</p>\n<ul>\n<li>The string can contain any characters.</li>\n<li>If there's an odd number of characters in the string, ignore the center character.</li>\n</ul>",
   "javascript": {
     "tests": [
       {
-        "text": "<p><code>vaporwave('hello world')</code> should return a <code>H  E  L  L  O  W  O  R  L  D</code>.</p>",
-        "testString": "assert.strictEqual(vaporwave('hello world'), 'H  E  L  L  O  W  O  R  L  D');"
+        "text": "<p><code>isBalanced(\"racecar\")</code> should return <code>true</code>.</p>",
+        "testString": "assert.isTrue(isBalanced(\"racecar\"));"
+      },
+      {
+        "text": "<p><code>isBalanced(\"lorem ipsum\")</code> should return <code>true</code>.</p>",
+        "testString": "assert.isTrue(isBalanced(\"lorem ipsum\"));"
+      },
+      {
+        "text": "<p><code>isBalanced(\"kitty ipsum\")</code> should return <code>false</code>.</p>",
+        "testString": "assert.isFalse(isBalanced(\"kitty ipsum\"));"
+      },
+      {
+        "text": "<p><code>isBalanced(\"string\")</code> should return <code>false</code>.</p>",
+        "testString": "assert.isFalse(isBalanced(\"string\"));"
+      },
+      {
+        "text": "<p><code>isBalanced(\" \")</code> should return <code>true</code>.</p>",
+        "testString": "assert.isTrue(isBalanced(\" \"));"
+      },
+      {
+        "text": "<p><code>isBalanced(\"abcdefghijklmnopqrstuvwxyz\")</code> should return <code>false</code>.</p>",
+        "testString": "assert.isFalse(isBalanced(\"abcdefghijklmnopqrstuvwxyz\"));"
+      },
+      {
+        "text": "<p><code>isBalanced(\"123a#b!E&#x26;*456-o.U\")</code> should return <code>true</code>.</p>",
+        "testString": "assert.isTrue(isBalanced(\"123a#b!E&*456-o.U\"));"
       }
     ],
     "challengeFiles": [
       {
-        "fileKey": "scriptjs",
-        "contents": "function vaporwave(str) {\n\n  return str;\n}\n\nconsole.log(vaporwave('hello world'));"
+        "contents": "function isBalanced(str) {\n\n  return str;\n}",
+        "fileKey": "scriptjs"
       }
-    ],
-    "disableLoopProtectTests": true
+    ]
   },
   "python": {
     "tests": [
       {
-        "text": "<p><code>vaporwave(\"hello world\")</code> should return <code>H  E  L  L  O  W  O  R  L  D</code>.</p>",
-        "testString": "({\n  test: () => {\n    runPython(`\nfrom unittest import TestCase\n\nTestCase().assertEqual(vaporwave(\"hello world\"), 'H  E  L  L  O  W  O  R  L  D')`);\n  }\n})"
+        "text": "<p><code>isBalanced(\"racecar\")</code> should return <code>True</code>.</p>",
+        "testString": "({test: () => { runPython(`\nfrom unittest import TestCase\nTestCase().assertTrue(is_balanced(\"racecar\"))`);\n}})"
+      },
+      {
+        "text": "<p><code>isBalanced(\"lorem ipsum\")</code> should return <code>True</code>.</p>",
+        "testString": "({test: () => { runPython(`\nfrom unittest import TestCase\nTestCase().assertTrue(is_balanced(\"lorem ipsum\"))`);\n}})"
+      },
+      {
+        "text": "<p><code>isBalanced(\"kitty ipsum\")</code> should return <code>False</code>.</p>",
+        "testString": "({test: () => { runPython(`\nfrom unittest import TestCase\nTestCase().assertFalse(is_balanced(\"kitty ipsum\"))`);\n}})"
+      },
+      {
+        "text": "<p><code>isBalanced(\"string\")</code> should return <code>False</code>.</p>",
+        "testString": "({test: () => { runPython(`\nfrom unittest import TestCase\nTestCase().assertFalse(is_balanced(\"string\"))`);\n}})"
+      },
+      {
+        "text": "<p><code>isBalanced(\" \")</code> should return <code>True</code>.</p>",
+        "testString": "({test: () => { runPython(`\nfrom unittest import TestCase\nTestCase().assertTrue(is_balanced(\" \"))`);\n}})"
+      },
+      {
+        "text": "<p><code>isBalanced(\"abcdefghijklmnopqrstuvwxyz\")</code> should return <code>False</code>.</p>",
+        "testString": "({test: () => { runPython(`\nfrom unittest import TestCase\nTestCase().assertFalse(is_balanced(\"abcdefghijklmnopqrstuvwxyz\"))`);\n}})"
+      },
+      {
+        "text": "<p><code>isBalanced(\"123a#b!E&#x26;*456-o.U\")</code> should return <code>True</code>.</p>",
+        "testString": "({test: () => { runPython(`\nfrom unittest import TestCase\nTestCase().assertTrue(is_balanced(\"123a#b!E&*456-o.U\"))`);\n}})"
       }
     ],
     "challengeFiles": [
       {
-        "fileKey": "mainpy",
-        "contents": "def vaporwave(str):\n\n    return str\n\nprint(f'\\n{vaporwave(\"hello world\")}')"
+        "contents": "def is_balanced(s):\n\n    return s\n",
+        "fileKey": "mainpy"
       }
     ]
   }
