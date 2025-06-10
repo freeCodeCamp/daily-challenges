fad65624acf71f05f3d85e918e8b4e5ae1604d1b
moT01 <20648924+moT01@users.noreply.github.com>
2025-06-10 12:09:57 -0500

    feat: add more features

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 To Run:
-- Copy `sample.env` to `.env`
+- Copy `sample.env` to `.env` - if seeding the development database, set `ALLOW_SEED` to `true`
 - Install dependencies with `pnpm i`
-- Make sure mongo is running, and seed the database with `pnpm run seed`. It will seed the placeholder challenge for today, tomorrow, and the five previous days in US Central time. Change the number in `database/seed.js` to seed more days.
+- Make sure mongo is running, and seed the database with `pnum run seed`. It will seed the placeholder challenge for the numbers of days according to the variables in the seed script
 - Run the server with `pnpm run develop`
 - Get challenge info at `localhost:3000/api/daily-challenge/date/:date` - e.g. `http://localhost:3000/api/daily-challenge/date/3-20-2025` <- note the date format: `M-D-YYYY`
--- src/routes.ts
+++ src/routes.ts
@@ -1,6 +1,6 @@
 import { Router } from 'express';
 
-import { dailyChallenges } from './db';
+import { dailyCodingChallenges } from './db';
 import { getTodayUsCentral } from './utils/helpers';
 import { handleError, HttpError } from './utils/errors';
 
@@ -18,26 +18,20 @@ router.get('/api/daily-challenge/date/:date', async (req, res) => {
     const [month, day, year] = date.split('-').map(Number) as [number, number, number];
     const challengeDate = new Date(Date.UTC(year, month - 1, day));
 
-    if (
-      challengeDate.getUTCFullYear() !== year ||
-      challengeDate.getUTCMonth() !== month - 1 ||
-      challengeDate.getUTCDate() !== day
-    ) {
+    if (challengeDate.getUTCFullYear() !== year || challengeDate.getUTCMonth() !== month - 1 || challengeDate.getUTCDate() !== day) {
       throw new HttpError(400, `Invalid calendar date: "${date}"`);
     }
 
-    const challenge = await dailyChallenges.findOne({ date: challengeDate });
+    const challenge = await dailyCodingChallenges.findOne({ date: challengeDate });
     const todayUsCentral = getTodayUsCentral();
 
     // do not send challenge back if it's for a future date (relative to today US Central)
     if (challenge && challenge.date <= todayUsCentral) {
       res.status(200).json(challenge);
       return;
-
     } else {
       throw new HttpError(404, `Challenge not found for "${date}"`);
     }
-
   } catch (err) {
     handleError(err, res);
   }
@@ -47,7 +41,7 @@ router.get('/api/daily-challenge/date/:date', async (req, res) => {
 router.get('/api/daily-challenge/all', async (req, res) => {
   try {
     const todayUsCentral = getTodayUsCentral();
-    const challenges = await dailyChallenges.find({ date: { $lte: todayUsCentral } }, { projection: { challengeId: 1, date: 1, _id: 0 } }).toArray();
+    const challenges = await dailyCodingChallenges.find({ date: { $lte: todayUsCentral } }, { projection: { challengeId: 1, date: 1, _id: 0 } }).toArray();
 
     if (challenges.length > 0) {
       res.status(200).json(challenges);
@@ -55,7 +49,28 @@ router.get('/api/daily-challenge/all', async (req, res) => {
     } else {
       throw new HttpError(404, 'No Challenges found');
     }
+  } catch (err) {
+    handleError(err, res);
+  }
+});
 
+// return the { date } of the newest challenge so we can check how many challenges are available
+router.get('/api/daily-challenge/newest', async (req, res) => {
+  try {
+    const newestChallenge = await dailyCodingChallenges.findOne(
+      {},
+      {
+        sort: { date: -1 },
+        projection: { date: 1, _id: 0 },
+      }
+    );
+
+    if (!newestChallenge) {
+      throw new HttpError(204, 'No Challenge found');
+    }
+
+    res.status(200).json(newestChallenge);
+    return;
   } catch (err) {
     handleError(err, res);
   }
